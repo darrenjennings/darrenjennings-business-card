d78d4729b3be8fc5e6dc0b348722e0fab74b1c43
darrenjennings <darren@konghq.com>
2021-02-07 13:49:28 -0500

    email

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 #! /usr/bin/env node
 
-const boxen = require("boxen");
+const boxen = require('boxen')
 // chalk, but without chalk
-const _c = require('util').inspect.colors;
-Object.keys(_c).forEach(c =>global[c] = s =>`\x1b[${_c[c][0]}m${s}\x1b[${_c[c][1]}m`);
+const _c = require('util').inspect.colors
+Object.keys(_c).forEach(c =>global[c] = s =>`\x1b[${_c[c][0]}m${s}\x1b[${_c[c][1]}m`)
 
 const lines = [
-  bold("Darren Jennings"),
-  "Tech Lead at Kong",
-  "",
-  bold("       Web: ") + blue("https://darrenjennings.github.io"),
-  bold("   Twitter: ") + blue("https://twitter.com/darrenjennings"),
-  bold("    GitHub: ") + blue("https://github.com/darrenjennings"),
-  bold("  LinkedIn: ") + blue("https://www.linkedin.com/in/darren-jennings")
-];
+  bold('Darren Jennings'),
+  italic('Tech Lead at Kong'),
+  '',
+  bold('       Web: ') + blue('https://darrenjennings.dev'),
+  bold('   Twitter: ') + blue('https://twitter.com/darrenjennings'),
+  bold('    GitHub: ') + blue('https://github.com/darrenjennings'),
+  bold('  LinkedIn: ') + blue('https://www.linkedin.com/in/darren-jennings'),
+  '',
+  'Say Hello: ', italic(green('dmjenn02@gmail.com'))
+]
 
-const message = lines.join("\n");
+const message = lines.join('\n')
 
 const box = boxen(message, {
-  borderColor: "blue",
-  borderStyle: "round",
+  borderColor: 'blue',
+  borderStyle: 'round',
   margin: 1,
   padding: 1
-});
+})
 
-process.stdout.write(box + "\n");
+process.stdout.write(box + '\n')
